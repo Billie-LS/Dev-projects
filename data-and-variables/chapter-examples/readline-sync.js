8bfa5af869347b6f41e46ae7e2f09ab08dbfbd0d
Billie-LS <lskylizard@yahoo.com>
2024-01-08 15:01:48 -0600

    readline-sync

--- data-and-variables/chapter-examples/readline-sync.js
+++ data-and-variables/chapter-examples/readline-sync.js
@@ -1,8 +1,5 @@
 const input = require('readline-sync');
 
-// let info = input.question("Question text... ");
-const input = require('readline-sync');
-
 let info = input.question("Please enter your age: ");
 //The user enters 25.
 
