9f75b223da693ff7caa222ff4fa62631d82f2ad1
Billie-LS <lskylizard@yahoo.com>
2024-01-10 22:00:09 -0600

    Exercises runtime, syntax, logic erors

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -1,23 +1,22 @@
-// Now consider both if/else blocks together (keeping the added console.log lines). 
-// Run the code and examine the output. 
+// Let’s break the code down into smaller chunks.
+// Now consider the second if/else block. 
+// Add another console.log(launchReady) after this block and run the program.
 
-// Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
+// Given the values for crewStatus and computerStatus, should launchReady be true or false after the check?  
 // Is the program behaving as expected?
 
 let launchReady = false;
-let fuelLevel = 17000;
+// let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
 
-if (fuelLevel >= 20000) {
-   console.log('Fuel level cleared.');
-   launchReady = true;
-} else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
-}
-
-console.log("launchReady = ", launchReady);
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
@@ -27,7 +26,7 @@ if (crewStatus && computerStatus === 'green'){
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log(launchReady)
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -2,7 +2,8 @@
 // Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result. 
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
-let launchReady = false;
+let launchReady = '';
+let crewReady = '';
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
@@ -10,19 +11,27 @@ let computerStatus = 'green';
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
    launchReady = true;
+   console.log(`launchReady = ${launchReady}\n`);
 } else {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
+   console.log(`launchReady = ${launchReady}\n`);
 }
 
-console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
+   console.log(`crewReady =  ${crewReady}\n`);
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
+   console.log(`crewReady =  ${crewReady}\n`);
 }
 
-console.log("launchReady = ", launchReady);
+if (launchReady && crewReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
