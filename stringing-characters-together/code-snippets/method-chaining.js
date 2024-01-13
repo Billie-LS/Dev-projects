81f54a5f8acedc1346d2196bb4b5b9ea2fd271b7
Billie-LS <lskylizard@yahoo.com>
2024-01-13 17:44:03 -0600

    method-chaining

--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -1,11 +1,41 @@
 //String methods can be combined in a process called method chaining.
 
 let word = 'JavaScript';
+// stringName.toUpperCase()
+console.log(word.toUpperCase()); //Returns ``JAVASCRIPT``
 
-console.log(word.toUpperCase());
-//Returns ``JAVASCRIPT``
+// stringName.slice(i)
+console.log(word.slice(4));  // returns Script
+console.log(word.slice(4).toUpperCase());  
+//      first, .slice returns Script , 
+//      then .toUpperCase() returns SCRIPT, so answer is SCRIPT
 
-//What does ``word.slice(4).toUpperCase()`` return?
+// Experimentation
 
+// stringName.slice(i,j)
+console.log(word.slice(4,6));  // returns Sc
+console.log(word.slice(4,6).toUpperCase());  
+//      first, .slice returns Sc , 
+//      then .toUpperCase() returns SC, so answer is SC
 
-//Experiment with other combinations (chains) of string methods.
+// stringName.replace('searchChar', 'replacementChar')
+console.log(word.replace('Java', 'Tea'));  // returns TeaScript
+
+// stringName.charAt()
+console.log(word.charAt(4));  // returns S
+console.log(word.charAt(4).toLowerCase());  // returns s
+
+// Check Your Understanding
+let language = "JavaScript";
+language.replace('J', 'Q');  
+console.log(language.replace('J', 'Q'));  // Qavascript
+language.slice(0,5); 
+console.log(language.slice(0,5)); // JavaS
+console.log(language);  // JavaScript
+
+console.log(language.slice(1,6));  // avaSc
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);  // returns The 'LaunchCode Foundation' without lead or trail empty space
