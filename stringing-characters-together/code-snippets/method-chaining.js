91af0c9284496dfced7f5b01948ed0ea9b7c3d9c
Billie-LS <lskylizard@yahoo.com>
2024-01-13 22:38:34 -0600

    strings examples and exercises

--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -39,3 +39,23 @@ let org = "  The LaunchCode Foundation ";
 let trimmed = org.trim();
 
 console.log(trimmed);  // returns The 'LaunchCode Foundation' without lead or trail empty space
+
+const unicodeValue = 65; // Unicode value for 'A' 
+const char = String.fromCharCode(unicodeValue); 
+console.log(char); // Output: 'A'
+
+
+let codes = [76, 97, 117, 110, 99, 104, 67, 111, 100, 101];
+
+let characters = String.fromCharCode(codes[0]) + String.fromCharCode(codes[1])
+                + String.fromCharCode(codes[2]) + String.fromCharCode(codes[3])
+                + String.fromCharCode(codes[4]) + String.fromCharCode(codes[5])
+                + String.fromCharCode(codes[6]) + String.fromCharCode(codes[7])
+                + String.fromCharCode(codes[8]) + String.fromCharCode(codes[9]);
+console.log(characters);
+
+let fullWord = String.fromCharCode(...codes);  // use spread operator `...` instead
+console.log(fullWord);
+
+const degreeSymbol = String.fromCharCode(0x00B0);  //  0x00B0 is hexadecimal of the Unicode value for the degree symbol.
+console.log(degreeSymbol); // Output: °
