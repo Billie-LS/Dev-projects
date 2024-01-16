c3431e405b72ae5c066c4a9a1be7e25483d14088
Billie-LS <lskylizard@yahoo.com>
2024-01-16 09:13:19 -0600

    npm install to studio2, studio2 coding

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,8 +3,29 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+strV = str.slice(3) + 'Lau'; 
+console.log(str);  // nchCodeLau
+
+// strV = str.slice(3).concat('Lau');
+// console.log(str);    // nchCodeLau
+
+// strV = str.slice(3);  // nchCode
+// strV = str.replace('nchCode', 'nchCodeLau');
+// console.log(str); // nchCodeLau
+
+// strV = str.slice(3).replace(/nchCode/g, 'nchCodeLau'); // regular expression '/g' is the global flag
+// console.log(str);    // nchCodeLau
+
+// strV = str.slice(3).replace(/.*/, 'nchCodeLau');  // regular expression '/.*' is the entire string
+// console.log(str);  // nchCodeLau
+
+// strV = (`${str.slice(3)}Lau`);
+// console.log(str); // nchCodeLau
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The original is: ${str}.
+Sliced and diced is: ${strV}.`);
+
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
