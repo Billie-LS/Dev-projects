39fbfcc1a9386b5d8099e739f679b9b256a04e70
Billie-LS <lskylizard@yahoo.com>
2024-01-16 20:04:33 -0600

    Studio 2- Bonus Mission: adding Switch

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -39,15 +39,60 @@ console.log(cargoHold);
     Use the 'includes' method to check if the cabinet contains the selected item, 
     then print “Cabinet ____ DOES/DOES NOT contain ____.”
 */
-let cabinetSelectInputB = Number(input.question('Choose a number from 0 to 3 to indicate which cabinet to access: '));
-if (cabinetSelectInputB <= 3 && cabinetSelectInputB >= 0){
-    console.log(`The contents of the cabinet you selected are: \n\t${cargoHold[cabinetSelectInputB]}.`);
-    let cabinetSelectInputC = input.question(`Please enter the item name you want from cabinet ${cargoHold[cabinetSelectInputB]}: `);
-    if (cargoHold[cabinetSelectInputB].includes(cabinetSelectInputC)){
-        console.log(`Cabinet ${cabinetSelectInputB} DOES contain ${cabinetSelectInputC}.`);
-    }
-}else{
-    console.log(`Cabinet ${cargoHold[cabinetSelectInput]} DOES/DOES NOT contain ${cabinetSelectInputC}.`);
-}
+// let cabinetSelectInputB = Number(input.question('Choose a number from 0 to 3 to indicate which cabinet to access: '));
+// if (cabinetSelectInputB <= 3 && cabinetSelectInputB >= 0){
+//     console.log(`The contents of the cabinet you selected are: \n\t${cargoHold[cabinetSelectInputB]}.`);
+//     let cabinetSelectInputC = input.question(`Please enter the item name you want from cabinet ${cargoHold[cabinetSelectInputB]}: `);
+//     if (cargoHold[cabinetSelectInputB].includes(cabinetSelectInputC)){
+//         console.log(`Cabinet ${cabinetSelectInputB} DOES contain ${cabinetSelectInputC}.`);
+//     }
+// }else{
+//     console.log(`Cabinet ${cargoHold[cabinetSelectInputB]} DOES/DOES NOT contain ${cabinetSelectInputC}.`);
+// }
 // console.log(`The contents of the cabinet you selected are: ${cargoHold[cabinetSelectInputB]} `);
 // let cabinetSelectInputC = Number(input.question(`Please enter the item name of the item you want from cabinet ${cabinetSelectInputB}: `));
+
+// let cabinetSelectInputB = Number(input.question('Choose a number from 0 to 3 to indicate which cabinet to access: '));
+
+// if (cabinetSelectInputB <= 3 && cabinetSelectInputB >= 0) {
+//     console.log(`The contents of the cabinet you selected are: \n\t${cargoHold[cabinetSelectInputB]}.`);
+//     let cabinetSelectInputC = input.question(`Please enter the item name you want from cabinet ${cabinetSelectInputB}: `);
+
+//     if (cargoHold[cabinetSelectInputB].includes(cabinetSelectInputC)) {
+//         console.log(`Cabinet ${cabinetSelectInputB} DOES contain ${cabinetSelectInputC}.`);
+//     } else {
+//         console.log(`Cabinet ${cabinetSelectInputB} DOES NOT contain ${cabinetSelectInputC}.`);
+//     }
+// } else {
+//     console.log(`Invalid cabinet selection. Please choose a number between 0 and 3.`);
+// }
+
+// using 'switch' instead
+let cabinetSelectInputB = Number(input.question('Choose a number from 0 to 3 to indicate which cabinet to access: '));
+
+// Using switch for different cases of cabinetSelectInputB
+switch (cabinetSelectInputB) {
+    // Cases for valid cabinet numbers (0, 1, 2, 3)
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+        // Display contents of selected cabinet
+        console.log(`The contents of the cabinet you selected are: \n\t${cargoHold[cabinetSelectInputB]}.`);
+        
+        // Prompt the user input item name they want from cabinet
+        let cabinetSelectInputC = input.question(`Please enter the item name you want from cabinet ${cabinetSelectInputB}: `);
+
+        // Check if the selected cabinet contains the specified item
+        if (cargoHold[cabinetSelectInputB].includes(cabinetSelectInputC)) {
+            console.log(`Cabinet ${cabinetSelectInputB} DOES contain ${cabinetSelectInputC}.`);
+        } else {
+            console.log(`Cabinet ${cabinetSelectInputB} DOES NOT contain ${cabinetSelectInputC}.`);
+        }
+        break;
+
+    // Default case for invalid cabinet numbers
+    default:
+        console.log(`Invalid cabinet selection. Please choose a number between 0 and 3.`);
+        break;
+}
