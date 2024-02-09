b40cfd292820f347bf05aade9d2a86787541a0a0
Billie-LS <lskylizard@yahoo.com>
2024-02-08 20:59:45 -0600

    unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,13 @@
 
 let launchcode = {
-  
-}
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function() {
+        // Placeholder method, will be implemented later
+    }
+};
 
-module.exports = launchcode;
+module.exports = { launchcode };
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,30 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const test = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
-  
+  // TODO Write your unit tests here!
+  it('should have property organization of value "nonprofit" ', function() {
+    let result = test.launchcode.organization; // test for property organization
+    expect(result).toBe('nonprofit'); // Expected return 'nonprofit'
 });
+
+it('should have property executiveDirector of value "Jeff" ', function() {
+  let result = test.launchcode.executiveDirector; // test for property executive director
+  expect(result).toBe('Jeff'); // Expected return 'Jeff'
+});
+
+it('should have property percentageCoolEmployees of value 100 ', function() {
+  let result = test.launchcode.percentageCoolEmployees; // test for property percentageCoolEmployees 
+  expect(result).toBe(100); // Expected return 100
+});
+
+it('should have property programsOffered of value ["Web Development", "Data Analysis", "Liftoff"] ', function() {
+  let result = test.launchcode.programsOffered; // test for property programsOffered 
+  expect(result).toEqual(['Web Development', 'Data Analysis', 'Liftoff']); // Expected return ['Web Development', 'Data Analysis', 'Liftoff']
+});
+
+
+
+});
+
