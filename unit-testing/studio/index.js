e6b7116b63b60466f56b8e64b7093e9fe283e69f
Billie-LS <lskylizard@yahoo.com>
2024-02-09 17:45:10 -0600

    refactoring pass index.js TDD unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,13 +1,27 @@
-
 let launchcode = {
     organization: "nonprofit",
     executiveDirector: "Jeff",
     percentageCoolEmployees: 100,
     programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
-    launchOutput: function() {
-        // Placeholder method, will be implemented later
+    launchOutput: function(num) {
+        if (num % 2 === 0 && num % 3 === 0) {
+            return 'LaunchCode!';
+        }
+        if (num % 2 === 0 && num % 5 === 0) {
+            return 'Launch Rocks! (CRASH!!!!)';
+        }
+        if (num % 2 === 0) {
+            return 'Launch!';
+        }
+        if (num % 3 === 0) {
+            return 'Code!';
+        }
+        if (num % 5 === 0) {
+            return 'Rocks!';
+        }
+        return 'Bang!';
     }
 };
 
-module.exports = { launchcode };
 
+module.exports = { launchcode };
